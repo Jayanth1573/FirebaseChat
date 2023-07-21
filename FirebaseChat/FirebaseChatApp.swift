b19ea8ad74951c480af59f98af713318863e0e95
Jayanth <jayanthambaldhage15@gmail.com>
2023-07-22 00:16:24 +0530

    Initial Commit

---
+++ FirebaseChat/FirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatApp.swift
+//  FirebaseChat
+//
+//  Created by Jayanth Ambaldhage on 22/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
