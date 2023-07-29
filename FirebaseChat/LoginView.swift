c17c0a2d5691f632da36b3bed0e279ec12081cff
Jayanth <jayanthambaldhage15@gmail.com>
2023-07-30 01:07:33 +0530

    Initialise Firebase Auth and Firebase Storage using Swift Package Manager to sign in and create user account.

--- FirebaseChat/LoginView.swift
+++ FirebaseChat/LoginView.swift
@@ -6,11 +6,30 @@
 //
 
 import SwiftUI
+import Firebase
+
+class FirebaseManager: NSObject{
+    let auth: Auth
+    
+    static let shared = FirebaseManager()
+    override init() {
+        FirebaseApp.configure()
+        self.auth = Auth.auth()
+        super.init()
+    }
+    
+}
 
 struct LoginView: View {
+
     @State var isLoginMode = false
     @State var email = ""
     @State var password = ""
+    
+//    init() {
+//        FirebaseApp.configure()
+//    }
+    
     var body: some View {
         NavigationView {
             ScrollView{
@@ -34,17 +53,19 @@ struct LoginView: View {
                                 .padding()
                         }
                     }
+                    
                     TextField("Email", text: $email)
                         .keyboardType(.emailAddress)
                         .autocapitalization(.none)
                         .padding(12)
                         .background(Color.white)
+                    
                     SecureField("Password", text: $password)
                         .padding(12)
                         .background(Color.white)
+                    
                     Button {
                         handleAction()
-                        
                     } label: {
                         HStack {
                             Spacer()
@@ -56,19 +77,40 @@ struct LoginView: View {
                         }
                         .background(Color.blue)
                     }
+                    
                 }
                 .padding()
             }
             .navigationTitle(isLoginMode ? "Login": "Create Account")
             .background(Color(white: 0, opacity: 0.05))
         }
+        .navigationViewStyle(StackNavigationViewStyle())
     }
     
     private func handleAction(){
         if isLoginMode {
-            print("Login using existing credentials")
+            LoginUser()
         } else {
-            print("Create a new account.")
+            CreateNewAccount()
+        }
+    }
+    
+    private func LoginUser() {
+        FirebaseManager.shared.auth.signIn(withEmail: email, password: password) { result, error in
+            if let error {
+                print("Error sigining in user: \(error)")
+            }
+            print("Succesfully logged in: \(result?.user.uid ?? "")")
+        }
+    }
+    
+    private func CreateNewAccount() {
+        FirebaseManager.shared.auth.createUser(withEmail: email, password: password) { result, error in
+            if let error {
+                print("Failed to create new user: \(error)")
+                return
+            }
+            print("Succesfully created user: \(result?.user.uid ?? "")")
         }
     }
 }
